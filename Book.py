5b747f0860361c740731c21ea8b1c6a120a75c7e
Karp-636 <alanakarp05@gmail.com>
2026-06-22 20:13:10 -0600

    sumbitted Book_catalog

--- Book.py
+++ Book.py
@@ -1,1 +1,28 @@
-# your Book class goes here
+class Book:
+    total_books = 0
+
+    def __init__(self, title, author, page_count, genre):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+        Book.total_books += 1
+        self.id = Book.total_books
+
+    def describe(self):
+        print(f'"{self.title}" by {self.author} - {self.page_count} pages ({self.genre})')
+        print(f'{self.id} out of {self.total_books}')
+
+
+
+book1 = Book("Nineteen Eighty-Four", "Geogre Orwell", 328, "fiction")
+book2 = Book("Marked - The House of Night", "P.C. Cast and Kristin Cast", 304, "paranormal fantasy" )
+book3 = Book("Evermore - The Immortals", "Alyson Noel", 320, "paranormal fantasy")
+book4 = Book("The Lightning Thief - Percy Jackson & the Olympians", "Rick Riordan", 377, "greek mythology fantasy")
+
+
+
+book1.describe()
+book2.describe()
+book3.describe()
+book4.describe()
